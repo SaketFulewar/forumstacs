95d48ea2bcea1cbff3dc3cc39d59ae690ecdc772
Saket <saketfulewar02@gmai.com>
2022-03-07 01:13:36 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const popupContainer = document.querySelector(".popup-container");
+const popup = document.querySelector(".popup");
+const btnClose = document.querySelector(".btn-close");
+
+function popUp(){
+    popupContainer.style.display = "flex";
+    setTimeout(() => {
+        popupContainer.style.opacity = "1";
+        popup.style.transform = "scale(1)";
+    }, 500);
+}
+
+    btnClose.addEventListener("click",()=>{
+        popupContainer.style.opacity = "0";
+        popUp.style.display = "none";
+        setTimeout(() => {
+            popupContainer.style.display = "none";
+        }, 500);
+    })
+
+setTimeout(() => {
+    popUp();
+}, 500);
