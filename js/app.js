ddb30c3e4a8f45576916dbb3b875a3ce71a307df
Saket <saketfulewar02@gmai.com>
2022-03-10 01:34:33 +0530

    Responsive Edition

--- js/app.js
+++ js/app.js
@@ -3,19 +3,17 @@ const popupContainer = document.querySelector(".popup-container");
 const popup = document.querySelector(".popup");
 const btnClose = document.querySelector(".btn-close");
 
-body.style.overflowY = "hidden";
-function popUp(){
-    popupContainer.style.display = "flex";
-    setTimeout(() => {
-        popupContainer.style.opacity = "1";
-        popup.style.transform = "scale(1)";
-    }, 500);
-}
+// function popUp(){
+//     popupContainer.style.display = "flex";
+//     setTimeout(() => {
+//         popupContainer.style.opacity = "1";
+//         popup.style.transform = "scale(1)";
+//     }, 500);
+// }
 
     btnClose.addEventListener("click",()=>{
         // popupContainer.style.opacity = "0";
         popupContainer.style.display = "none";
-        body.style.overflowY = "scroll";
         popUp.style.display = "none";
     })
 
